c56a04c2861f948efeac3b75dc84f78c27811f53
Farras Timorremboko <Farras>
2021-08-17 11:04:03 +0700

    update skeleton

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"SC/auth"
+	"SC/config"
+	"SC/route"
+	"fmt"
 
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	config.Init_DB()
+	config.InitPort()
+	auth.LogMiddlewares((e))
+	route.New(e)
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", config.HTTP_PORT)))
 }
