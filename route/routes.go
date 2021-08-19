146127875b64690d767080c940bc1e3330661569
Farras Timorremboko <Farras>
2021-08-19 09:26:02 +0700

    update routes

--- route/routes.go
+++ route/routes.go
@@ -25,13 +25,11 @@ func New(e *echo.Echo) {
 	//USER PROFILE
 	eJwt.GET("/users/:id", controller.ShowUserProfile)
 	eJwt.PUT("/users/:id", controller.EditUserProfile)
-	eJwt.DELETE("/users/:id", controller.DeleteUser)
-	eJwt.GET("users/peringkat", controller.ShowLeaderboards)
+	eJwt.GET("/users", controller.ShowLeaderboards)
 
 	//ADMIN PROFILE
 	eJwt.GET("/admin/:id", controller.ShowAdminProfile)
 	eJwt.PUT("/admin/:id", controller.EditAdminProfile)
-	eJwt.DELETE("/admin/:id", controller.DeleteAdmin)
 
 	//ADMIN FEATURES INPUT QUESTION
 	eJwt.POST("admin/soal", controller.SubmitQuestionAdmin)
@@ -42,20 +40,14 @@ func New(e *echo.Echo) {
 
 	//ADMIN FEATURE REVIEW SUBMITTED QUESTION FROM USER
 	eJwt.GET("admin/submit_soal/:kategori_materi_pelajaran_id", controller.ShowSubmittedQuestion) //SHOW QUESTIONS ARE NOTE REVIEWED BY CATEGORY
-	eJwt.PUT("admin/submit_soal/:id", controller.EditSubmitQuestion)                              //CLASSIFICATION THE LEVEL OF QUESTION (EASY, MEDIUM, HARD) AND APPROVAL THE QUESTION (APPROVED OR REJECT)
-	eJwt.POST("admin/submit_soal/soal/:id", controller.SendQuestion)                              //SEND SUBMITTED QUESTION TO QUESTION BOX AFTER REVIEW
-	eJwt.DELETE("admin/submit_soal/:id", controller.RejectQuestion)
+	eJwt.PUT("admin/submit_soal/:id", controller.EditSubmitQuestion)                              //APPROVAL THE QUESTION (APPROVED OR REJECT)
 
 	//USER FEATURE SUBMIT NEW QUESTION
 	eJwt.POST("users/submit_soal", controller.SubmitQuestion)
 
 	//USER FEATURE EXERCISE
-	eJwt.POST("users/:user_id/soal/:kategori_materi_pelajaran_id/:kesulitan", controller.GenerateRandomQuestion)
+	eJwt.POST("users/:user_id/soal", controller.GenerateRandomQuestion)
 	eJwt.GET("users/:user_id/soal/:set_soal_id", controller.ShowActiveQuestion)
-	eJwt.PUT("users/:user_id/soal/:set_soal_id/:nomor1/:nomor2/:nomor3/:nomor4/:nomor5".controller.AnswerQuestion)
-	eJwt.GET("users/:user_id/soal/:set_soal_id/score", controller.ShowScore)
+	eJwt.PUT("users/:user_id/soal/:set_soal_id".controller.AnswerQuestion)
 	eJwt.GET("users/:user_id/soal/:set_soal_id/solution", controller.ShowSolution)
-
-	//GET CATEGORY
-	eJwt.GET("users/:user_id/soal/:kategori_materi_pelajaran", controller.ShowCategory)
 }
